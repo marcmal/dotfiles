2278be06b9c33fc720ee2be992c2821b2b2aa861
marmal95 <marcin.malecki95@gmail.com>
2022-12-20 09:03:06 +0100

    Update work & theme

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,10 +1,7 @@
 my_config = {
-  use_powerline = true,
-  theme = 'onedark'
+  use_powerline = true
 }
 
 require('core.options')
 require('plugins').configure()
-require('core.theme')
 pcall(require, 'core.work')
-
